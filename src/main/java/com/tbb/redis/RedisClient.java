40354a7ff86303c5c64c7316eb3c583672c2a9f7
tubingbing <tubingbing@jd.com>
2017-03-21 09:11:50 +0800

    init

--- src/main/java/com/tbb/redis/RedisClient.java
+++ src/main/java/com/tbb/redis/RedisClient.java
@@ -1,16 +1,21 @@
 package com.tbb.redis;
 
+import com.tbb.algorithm.RedisLua;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 import redis.clients.util.Hashing;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * 工具类--获取redis连接池
+ *
  * @author: tubingbing
  * @Date: 2017/3/20
  * @Time: 13:13
@@ -19,28 +24,59 @@ public class RedisClient {
 
     private static ShardedJedisPool jedisPool;
 
+    private static final String DEFAULT_IP = "127.0.0.1";
+
+    private static final String DEFAULT_PORT = "6379";
+
     static {
-        List<JedisShardInfo> shardInfoList = new ArrayList<JedisShardInfo>();
-        for(int i=0;i<10;i++){
-            JedisShardInfo hostAndPort = new JedisShardInfo("127.0.0.1",6379);
-            shardInfoList.add(hostAndPort);
+        try {
+            String path = RedisLua.class.getResource("/").getPath();
+            FileInputStream inputStream = new FileInputStream(new File(path + "limit.lua"));
+            Properties perties = new Properties();
+            perties.load(inputStream);
+            String[] ips = perties.getProperty("redis.ip", DEFAULT_IP).split(",");
+            String[] ports = perties.getProperty("redis.port", DEFAULT_PORT).split(",");
+            List<JedisShardInfo> shardInfoList = new ArrayList<JedisShardInfo>();
+            int length = Math.max(ips.length, ports.length);
+            for (int i = 0; i < length; i++) {
+                String ip = ips[i];
+                String port = ports[i];
+                if (ip == null) {
+                    ip = DEFAULT_IP;
+                }
+                if (port == null) {
+                    port = DEFAULT_PORT;
+                }
+                JedisShardInfo shardInfo = new JedisShardInfo(ip, Integer.parseInt(port));
+                shardInfoList.add(shardInfo);
+            }
+            jedisPool = new ShardedJedisPool(new GenericObjectPoolConfig(), shardInfoList, Hashing.MURMUR_HASH);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        jedisPool = new ShardedJedisPool(new GenericObjectPoolConfig(),shardInfoList, Hashing.MURMUR_HASH);
     }
 
     /**
      * 获取连接
+     *
      * @return
      */
-    public static ShardedJedis getShardedJedis(){
+    public static ShardedJedis getShardedJedis() {
+        if (jedisPool == null) {
+            return null;
+        }
         return jedisPool.getResource();
     }
 
     /**
      * 释放连接
+     *
      * @param jedis
      */
-    public static void close(ShardedJedis jedis){
+    public static void close(ShardedJedis jedis) {
+        if (jedisPool == null) {
+            return;
+        }
         jedisPool.returnResource(jedis);
     }
 
